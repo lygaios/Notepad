405dc3a9f3ca49100206f7f9a47b0adf47262f79
Joshua Brunke <joshuabrunke1@gmail.com>
2025-03-12 16:30:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+let notes = [];
+let trashNotes = [];
+
+function init() {
+  getFromLocalStorage();
+  renderNotes();
+  renderTrashNotes();
+}
+
+function renderNotes() {
+  document.getElementById("content").innerHTML = "";
+  for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+    content.innerHTML += getNoteTemplate(indexNote);
+  }
+}
+
+function renderTrashNotes() {
+  document.getElementById("trash").innerHTML = "";
+  for (
+    let indexTrashNote = 0;
+    indexTrashNote < trashNotes.length;
+    indexTrashNote++
+  ) {
+    trash.innerHTML += getTrashNoteTemplate(indexTrashNote);
+  }
+}
+
+function getNoteTemplate(indexNote) {
+  return `
+      <div class="note-box">
+          <p>${indexNote + 1}: ${notes[indexNote]} </p>
+          <button class="button" id="delete" onclick="deleteNote(${indexNote})">Löschen</button>
+      </div>
+  `;
+}
+
+function getTrashNoteTemplate(indexTrashNote) {
+  return `
+        <div class="trash-box">
+            <p>${indexTrashNote + 1}: ${trashNotes[indexTrashNote]}</p>
+            <button class="button" id="restore" onclick="permaDeleteNote(${indexTrashNote})">Endgültig löschen</button>
+        </div>
+    `;
+}
+
+function addNote(indexNote) {
+  let noteInput = document.getElementById("note-input");
+  if (noteInput.value != "") {
+    notes.push(noteInput.value);
+    saveToLocalStorage(indexNote);
+  }
+  renderNotes(indexNote);
+  noteInput.value = "";
+}
+
+function deleteNote(indexNote) {
+  let trashNote = notes.splice(indexNote, 1);
+  trashNotes.push(trashNote[0]);
+  saveToLocalStorage();
+  renderNotes();
+  renderTrashNotes();
+}
+
+function permaDeleteNote(indexTrashNote) {
+  trashNotes.splice(indexTrashNote, 1);
+  saveToLocalStorage();
+  renderTrashNotes();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("trashNotes", JSON.stringify(trashNotes));
+}
+
+function getFromLocalStorage() {
+  if (localStorage.getItem("notes")) {
+    let storedNotes = JSON.parse(localStorage.getItem("notes"));
+    notes = storedNotes;
+  };
+  if (localStorage.getItem("notes")) {
+    let storedTrash = JSON.parse(localStorage.getItem("trashNotes"));
+    trashNotes = storedTrash;
+  }
+}
